c3ad84fb4e3bb0c066187f4a6f327aae1dfb9132
bnino <brayannir1993@gmail.com>
2021-12-08 14:53:23 -0500

    Modal restart password

--- src/components/navHome.js
+++ src/components/navHome.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Popover, Button, OverlayTrigger } from "react-bootstrap";
+import { Popover, Button, OverlayTrigger, Form, FloatingLabel } from "react-bootstrap";
 import 'jquery/dist/jquery.js'
 
+
 const Nav = () => {
-  return (
+  return <>
     <section
       className="container-fluid"
       style={{ height: "80px", boxShadow: "-10px -16px black" }}
@@ -27,6 +28,7 @@ const Nav = () => {
                 trigger="click"
                 key='bottom'
                 placement='left'
+                rootClose 
                 overlay={
                   <Popover id='popover'>
                     <Popover.Header
@@ -37,7 +39,7 @@ const Nav = () => {
                         
                     </Popover.Header>
                     <Popover.Body>
-                    <div class="form-floating mb-3">
+                    <div class="form-floating mb-1">
                         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
                         <label for="floatingInput">Email address</label>
                         </div>
@@ -46,16 +48,23 @@ const Nav = () => {
                         <label for="floatingPassword">Password</label>
                     </div>
                     <div className='mt-3 d-flex justify-content-center'>
-                        <a className='btn btn-primary' href="">
-                            Iniciar sesion
-                        </a>
+                      <Button variant="secondary" size="sm">
+                        Ingresar
+                      </Button>
                     </div>
 
-                    <div className = 'd-flex justify-content-center'>
-                        <a href="" className=''>
-                            crear una cuenta
-                        </a>
+                    <div className='d-flex justify-content-center mt-1'>
+                      <a className='link-primary' href='#' data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        ¿Olvidó su contraseña?
+                      </a>
+                      <resetPassword />
                     </div>
+                    <div className='d-flex justify-content-center mt-1'>
+                      <a className='link-info' href='#' >
+                        Registrarse
+                      </a>
+                    </div>
+
                     </Popover.Body>
                   </Popover>
                 }
@@ -66,7 +75,41 @@ const Nav = () => {
         </ul>
       </nav>
     </section>
-  );
+    <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="staticBackdropLabel">Recuperar contraseña</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Correo electrónico"
+                  className="mb-3"
+                >
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </FloatingLabel>
+                <Form.Text className="text-muted">
+                  Ingrese el correo registrado en la plataforma
+                </Form.Text>
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Recuperar
+              </Button>
+            </Form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </>;
 };
 
 export default Nav;
